c0f11118e25adaef2524814da5851ae35817e5f7
Ganesh <gs000@pm.me>
2023-02-12 09:43:33 +0530

    Update app.py

--- Flipkart_Review_Scrapper/app.py
+++ Flipkart_Review_Scrapper/app.py
@@ -12,7 +12,6 @@ CORS(app)
 
 
 def get_query(baseurl: str) -> str:
-
     '''Getting the Query in the search field.'''
     
     if request.method == 'GET':
@@ -24,7 +23,9 @@ def get_query(baseurl: str) -> str:
     
 
 
-def get_response(url: str):
+def get_response(url: str) -> str:
+    '''Return the string containing the HTML of url.''',
+    
     try:
         response = R.get(url)
     except ConnectionError:
@@ -32,23 +33,30 @@ def get_response(url: str):
     
     return response.text
 
-def html_parser(markup: str):
+def html_parser(markup: str) -> 'bs4.BeautifulSoup':
+    '''Convert string into a Parsed HTML object.'''
+    
     return BS(markup, 'html.parser')
 
-def html_tag_finder(html_parsed: object, tag_name: str, identifier: dict):
+def html_tag_finder(html_parsed: 'bs4.BeautifulSoup', tag_name: str, identifier: dict) -> 'list[bs4.BeautifulSoup]':
+    '''Return a list of matching HTML tags from a Soup Object.'''
+    
     return html_parsed.findAll(tag_name, identifier)
 
-def extract_links(tags: 'list[str]', baseurl: str, tag_identifier: str):
+def extract_links(tags: 'list[str]', baseurl: str, tag_identifier: str) -> 'list[str]':
+    '''Return a list of string after adding baseurl to a tag's identifier.'''
+    
     return [baseurl + tag[tag_identifier] for tag in tags]
 
-def get_reviews(url: str):
+def get_reviews(url: str) -> None:
+    '''Create a JSON file containing all the Reviews from Flipkart url.'''
+    
     data = []
     
     page = get_response(url)
     parsed_page = html_parser(page)
     product_name = html_tag_finder(parsed_page, 'span', {'class':'B_NuCI'})
     comments = html_tag_finder(parsed_page, 'div', {'class': '_16PBlm'})
-
 
     for comment in comments:
         
@@ -67,18 +75,21 @@ def get_reviews(url: str):
                 })
         except:
             pass
-
+    
+    # Dumping all the data into a File.
     with open(str(time.time_ns())+'.json', 'w') as file:
         json.dump(data, file)
 
 
-def all_reviews():
+def all_reviews(fileName: str = 'result.json', delete_reviews: bool = True) -> 'list[dict]':
+    '''Return a list of dictonary and store a JSON file after merging all the Review JSON files.'''
     result = []
     for file in os.listdir('./'):
         if file.endswith('.json'):
             with open(file) as f:
                 data = json.load(f)
-            os.remove(file)
+            if delete_reviews:
+                os.remove(file)
             result.extend(data)
     
     with open('./result.json', 'w') as file:
@@ -93,17 +104,26 @@ def homepage():
 @app.route('/review', methods = ['GET', 'POST'])
 @cross_origin()
 def reviews():
+    # Getting the Query
     query = get_query(baseurl = 'https://www.flipkart.com/search?q=')
+    
+    # Getting the response of query
     page = get_response(query)
+    
+    # Parsing the response into Soup Object
     parsed_page = html_parser(page)
 
-
+    # Find links tag from the Parsed HTML
     boxes = html_tag_finder(parsed_page, 'a', {'class': "_1fQZEK"})
+    
+    # Extracting links from tags
     product_links = extract_links(boxes, 'https://www.flipkart.com', 'href')
-
+    
+    # Running all the get_reviews parallely for all the links
     with mp.Pool() as pool:
         pool.map(get_reviews, product_links)
     
+    # Getting all the Reviews
     result = all_reviews()
 
     if request.method == 'GET':
@@ -111,19 +131,10 @@ def reviews():
     elif request.method == 'POST':
         return jsonify(json.dumps(result))
         
-
-
         
 
 
-
-
-
-
-
-
-
-
-
+        
+        
 if __name__ == '__main__':
     app.run(host = '0.0.0.0', port = 8000, debug = True)
